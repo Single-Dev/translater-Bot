76ccdfff5b0b41d3493a7d6efe852c5bea6d652b
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-09-25 22:38:09 +0500

    update oxford.py

--- oxford.py
+++ oxford.py
@@ -5,8 +5,14 @@ app_key = "ed12860d963de6f07e7cb2a5e22e51f2"
 language = "en-gb"
 
 
+
 def Lugat(word_id):
-    url = f"https://od-api.oxforddictionaries.com:443/api/v2/entries/{language}/{word_id}".lower()
+    url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
     r = requests.get(url, headers={"app_id": app_id, "app_key": app_key}) 
     res = r.json()
-
+    if 'error' in res.keys():
+        return False
+    output = {}
+    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]
+    print(senses)
+Lugat("Hello")
