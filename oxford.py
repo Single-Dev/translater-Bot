1de866c4d5994b258c781fa4f85816ca9b35ded7
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-09-27 15:19:20 +0500

    xatolar tuzatildi

--- oxford.py
+++ oxford.py
@@ -6,26 +6,27 @@ language = "en-gb"
 
 
 
-def Lugat(word_id):
+def getDefinitions(word_id):
     url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
     r = requests.get(url, headers={"app_id": app_id, "app_key": app_key}) 
     res = r.json()
-    # print(res)
     if 'error' in res.keys():
         return False
-    output = {}
-    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]
-    definitons = []
-    if sense in senses:
-        definitons.append(f"{sense['definitons'][0]}")
-    output['definitons'] = "\n".join(definitons)
 
-    if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get['audioFile']:
+    output = {}
+    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
+    definitions = []
+    for sense in senses:
+        definitions.append(f"👉{sense['definitions'][0]}")
+    output['definitions'] = "\n".join(definitions)
+
+    if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get('audioFile'):
         output['audio'] = res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
     return output
 
 if __name__ == '__main__':
-    print(Lugat("america"))
+    from pprint import pprint as print
+    print(getDefinitions("usa"))
 
 
 
